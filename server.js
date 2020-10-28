df22141368f53cb5c610874c75a307ec9d16fb75
jhill@gmail.com <jhillnj@gmail.com>
2020-10-28 16:25:14 -0400

    Setting Up Nodemon

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/',(req,res) =>{
-  res.send("Hello web 2")
-})
+/* handling all the parsing */
+app.use(bodyParser.json());
+
+app.post('/',(req,res) =>{
+  var email = req.body.email;
+  var amount = req.body.amount;
+
+  res.send({'amount': amount, 'email': email});
+});
 
 app.listen(3000,()=>{
   console.log('server is running on port 3000')
